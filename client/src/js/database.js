8d44f36405b496a6a74f9c377bd4c56a89a3526a
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-07-12 15:14:41 -0700

    removed 'return result' line from database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.put({ note: content });
+  const request = store.add({ note: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -53,7 +53,7 @@ export const getDb = async () => {
    // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  // return result;
 };
 
 initdb();
